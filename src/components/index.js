f24908e559393e47b0dd4939b524a6c1007dcfa8
Aloesapling <aloesapling@gmail.com>
2023-02-15 13:33:56 +0100

    Index change

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as Navbar } from './Navbar'
 export { default as Asside } from './Asside'
 export { default as Main } from './Main'
-export { default as Forecast  } from './Forecast'
