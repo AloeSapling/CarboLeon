9fa6753fc3404dda65d39e5d5a9fd9aba932da81
janekskr <janeczek.skrobot@gmail.com>
2023-02-19 15:30:41 +0100

    oczko

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import sponsor from '../pages/sponsorIMG/Logo-ETRONIX-rgb.png'
+import '../styles/sponsors.css'
 import sponsoroko from '../pages/sponsorIMG/okodesign.png'
 import { useTranslation } from 'react-i18next'
 
@@ -9,10 +10,10 @@ const Sponsors = () => {
     <section className="sContainer">
           <h1>{t("Sponsors.1")}</h1>
           <div className="sponsors">
-            <a href="https://www.e-tronix.eu/">
+            <a href="https://www.e-tronix.eu/" target="_blank">
               <img src={sponsor} alt="sponsor" />
             </a>
-            <a href="https://www.oko-design.com/">
+            <a href="https://www.oko-design.com/" target="_blank">
               <img src={sponsoroko} alt="sponsor" />
             </a>
           </div>
