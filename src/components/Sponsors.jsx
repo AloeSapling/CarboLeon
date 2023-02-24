a259bb3bfe91f173a05a8b46805064f609c9f016
janekskr <janeczek.skrobot@gmail.com>
2023-02-24 15:00:40 +0100

    zmiany na zdjęciach i optymalizacja slidera

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import sponsor from '../pages/sponsorIMG/Logo-ETRONIX-rgb.png'
 import '../styles/sponsors.css'
-import sponsoroko from '../pages/sponsorIMG/okodesign.png'
 import { useTranslation } from 'react-i18next'
 
 const Sponsors = () => {
@@ -10,12 +8,13 @@ const Sponsors = () => {
     <footer className="sContainer">
           <h1>{t("Sponsors.1")}</h1>
           <div className="sponsor">
-            <a href="https://www.e-tronix.eu/">
-              <img src={sponsor} alt="sponsor" />
+            <a href="https://www.e-tronix.eu/" rel="noreferrer" target="_blank">
+              <img src={"images/sponsorIMG/Logo-ETRONIX-rgb.webp"} alt="sponsor" />
             </a>
-            <a href="https://www.oko-design.com/">
-              <img src={sponsoroko} alt="sponsor" />
+            <a href="https://www.oko-design.com/" rel="noreferrer" target="_blank">
+              <img src={"images/sponsorIMG/okodesign.webp"} alt="sponsor" />
             </a>
+            
           </div>
         </footer>
   )
