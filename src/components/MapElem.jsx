02076669334a2dfd202e0c46de0597560d94b456
KartoszBoza <Bartoszkoza09@gmail.com>
2023-03-08 12:47:42 +0100

    Mapa upgrade

--- src/components/MapElem.jsx
+++ src/components/MapElem.jsx
@@ -26,8 +26,8 @@ export const MapElem = () => {
   delete L.Icon.Default.prototype._getIconUrl;
 
   L.Icon.Default.mergeOptions({
-    iconSize: [50, 75],
-    iconAnchor: [24, 75],
+    iconSize: [60, 80],
+    iconAnchor: [29, 80],
     iconRetinaUrl:
        "https://www.linkpicture.com/q/Logo_geolocation.png",
     iconUrl:
@@ -89,29 +89,29 @@ export const MapElem = () => {
           noWrap="true"
         />
         <LayersControl>
-          <LayersControl.BaseLayer name={t("layer.satelite")}>
+          <LayersControl.Overlay name={t("layer.satelite")}>
             <TileLayer
               url="http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
               subdomains={["mt0", "mt1", "mt2", "mt3"]}
               attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
               noWrap={true}
             />
-          </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name={t("layer.topographicMap")}>
+          </LayersControl.Overlay>
+          <LayersControl.Overlay name={t("layer.topographicMap")}>
             <TileLayer
               url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
               attribution='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
               noWrap={true}
             />
-          </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name={t("layer.airports")}>
+          </LayersControl.Overlay>
+          <LayersControl.Overlay name={t("layer.airports")}>
             <TileLayer
               url="https://tileserver.memomaps.de/tilegen/{z}/{x}/{y}.png"
               subdomains={["mt0", "mt1", "mt2", "mt3"]}
               attribution='Map <a href="https://memomaps.de/">memomaps.de</a> <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               noWrap={true}
             />
-          </LayersControl.BaseLayer>
+          </LayersControl.Overlay>
           <LayersControl.Overlay checked name={t("layer.airPollution")}>
             <TileLayer
               url={`https://tiles.waqi.info/tiles/usepa-aqi/{z}/{x}/{y}.png?token=${process.env.REACT_APP_API_KEY_WAQI}`}
--- src/styles/LeafletCSS&IMG/icon.js
+++ src/styles/LeafletCSS&IMG/icon.js
@@ -1,8 +1,8 @@
 import L from "leaflet";
 
 export default L.icon({
-  iconSize: [50, 75],
-  iconAnchor: [24, 75],
+  iconSize: [60, 80],
+  iconAnchor: [29, 80],
   popupAnchor: [2, -40],
   iconUrl: "https://www.linkpicture.com/q/Logo_geolocation.png",
   // shadowUrl: "https://unpkg.com/leaflet@1.6/dist/images/marker-shadow.png"
